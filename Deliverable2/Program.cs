b87cb29b1bad25e664fb5878ad05d7773b7cff23
kalindih1 <73810475+kalindih1@users.noreply.github.com>
2021-09-02 18:12:35 -0400

    Completed Deliverable 2

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool looping = true;
+
+            bool looped = false;
+
+            string input;
+
+            string previousInput = "";
+
+           while (looping)
+            {
+                if (looped)
+                {
+
+                    Console.WriteLine("What else would you like to say? ");
+
+                }
+                else
+                {
+
+                    Console.WriteLine("What do you wish to say? ");
+                    looped = true;
+                }
+                
+                
+                
+
+                input = Console.ReadLine().ToLower();
+
+                if (input == previousInput)
+                {
+                    Console.WriteLine("I'm sorry but you have already said that");
+                }
+
+
+                else if (input == "hello")
+                {
+
+                    Console.WriteLine("Hi good to see you ");
+                }
+
+                else if (input == "bye")
+                {
+
+                    Console.WriteLine("Good bye! ");
+
+                    looping = false;
+                }
+                else if (input == "sup")
+                {
+                    Console.WriteLine("I am good ");
+                }
+                else if (input == "hello there")
+                {
+
+                    Console.WriteLine("General Kenobi ");
+                }
+
+
+                previousInput = input;
+
+
+
+
+            }
+        }
+    }
+}
